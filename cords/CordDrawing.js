52af57241e748e3df8d6e58e9b0a54f998ebc994
Jack Bird <jack.bird@durham.ac.uk>
2018-12-19 11:43:56 +0000

    Added functions to find the luminosity of a line and reduce the luminosity of a line by a set amount, used these functions to draw the image using the cords of a circle

--- cords/CordDrawing.js
+++ cords/CordDrawing.js
@@ -25,6 +25,8 @@ class CordDrawing extends ImageHandler {
 	setup() {
 		super.setup()
 
+		this.counter = 0;
+
 		if(this.createCanvas) {
 			createCanvas(this.width, this.width);
 		}
@@ -34,8 +36,8 @@ class CordDrawing extends ImageHandler {
 		let dTheta = 2*Math.PI/this.nbPegs;
 		let id = 0;
 		for(let theta = 0; theta < 2*Math.PI; theta += dTheta) {
-			let x = this.width/2 + (this.width/2)*Math.cos(theta);
-			let y = this.width/2 + (this.width/2)*Math.sin(theta);
+			let x = this.scaledWidth/2 + (this.scaledWidth/2)*Math.cos(theta);
+			let y = this.scaledWidth/2 + (this.scaledWidth/2)*Math.sin(theta);
 			this.pegs.push({
 				x: x,
 				y: y,
@@ -50,11 +52,81 @@ class CordDrawing extends ImageHandler {
 	}
 
 	draw(g) {
+		if(this.counter > this.lines) {
+			return;
+		}
+
 		if(!g && !this.createCanvas) {
 			return false
 		}
+		let lums = [];
+		let ids = [];
+		for(let i = 0; i < this.nbPegs; i += 1) {
+			if(this.pegs.id != this.currentPeg.id) {
+				let testpeg = this.pegs[i];
+				lums.push(this.getAvgLumOfLine(
+					this.currentPeg.x, this.currentPeg.y,
+					testpeg.x, testpeg.y
+					));
+				ids.push(testpeg.id);
+			}
+		}
+
+		let largest = 0;
+		let id = 0;
+		for(let i = 0; i < lums.length; i += 1) {
+			if(lums[i]> largest) {
+				largest	= lums[i];
+				id = ids[i];
+			}
+		}
+
+		let nextPeg = this.pegs[id];
+
+		this.reduceLumOfLine(
+			this.currentPeg.x, this.currentPeg.y,
+			nextPeg.x, nextPeg.y,
+			255
+			);
+
+		let x0 = this.currentPeg.x * (1/this.sampling);
+		let y0 = this.currentPeg.y * (1/this.sampling);
+
+		let x1 = nextPeg.x * (1/this.sampling);
+		let y1 = nextPeg.y * (1/this.sampling);
+
+		this.currentPeg = nextPeg;
+
+		if(g) {
+			g.line(x0,y0,x1,y1);
+		} else {
+			line(x0,y0,x1,y1);
+		}
+		this.counter += 1;
+
 	}
 
+	getAvgLumOfLine(x0,y0,x1,y1) {
+		let points = CordDrawing.brasenhamPoints(x0,y0,x1,y1);
+
+		let t = 0;
+		for(let i = 0; i < points.length; i += 1) {
+			t += super.getLumAt(points[i].x, points[i].y);
+		}
+
+		return t/points.length;
+	}
+
+	reduceLumOfLine(x0,y0,x1,y1,reducer) {
+		let points = CordDrawing.brasenhamPoints(x0,y0,x1,y1);
+
+		for(let i = 0; i < points.length; i += 1) {
+			let x = points[i].x;
+			let y = points[i].y;
+			let newValue = super.getLumAt(x,y) - reducer > 0 ? super.getLumAt(x,y) - reducer : 0;
+			super.setLumAt(newValue,x,y);
+		}
+	} 
 
 	static brasenhamPoints(x0, y0, x1, y1){
 		x0 = Math.floor(x0);
